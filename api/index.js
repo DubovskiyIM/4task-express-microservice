fcb92e5009be064c44a50827d599b386b50c2cd0
IgnatDubovskiy <57371070+IgnatDubovskiy@users.noreply.github.com>
2020-10-14 09:49:50 +0300

    access control headers in res.set when json

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ module.exports = express => {
       };
       if (req.headers['content-type'] === 'application/json') {
         res.set({
-          ...headers,
+          'Access-Control-Allow-Origin': 'https://kodaktor.ru',
           'Content-Type': 'application/json'
         }).send({ result });
       } else res.set(headers).send(`${result}`);
